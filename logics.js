d7c2e93d38f562d578e738093f3365d4aebb1bcd
Бездетный Николай <Nekooolay@mail.ru>
2019-03-23 20:35:37 +0300

    Создание логики

---
+++ logics.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("gameCanvas"); //ссылка на элемент по его ID
+var context = canvas.getContext("2d"); //контекст рисования на холсте
+
+//установка игрового цикла
