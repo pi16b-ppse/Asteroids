afe3614c69497c3c0a9cd55ff8c2358598e9dd0b
Бездетный Николай <Nekooolay@mail.ru>
2019-03-23 20:36:04 +0300

    Создание игрового пространства

--- logics.js
+++ logics.js
@@ -1,4 +1,19 @@
+const FPS = 30;
+
 var canvas = document.getElementById("gameCanvas"); //ссылка на элемент по его ID
 var context = canvas.getContext("2d"); //контекст рисования на холсте
 
 //установка игрового цикла
+setInterval(update, 1000 / FPS);
+
+function update() {
+    //рисуем космос
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    //рисуем корабль
+
+    //поворот корабля
+
+    //движение корабля
+
+}
