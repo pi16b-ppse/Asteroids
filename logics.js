3c53c19b0dc23a519d6f6f4720ec1b6a4fe2597a
Бездетный Николай <Nekooolay@mail.ru>
2019-03-23 20:36:33 +0300

    Создание космического корабля

--- logics.js
+++ logics.js
@@ -1,19 +1,45 @@
-const FPS = 30;
+const FPS = 30; // кадров в секунду
+const SHIP_SIZE = 30; // высота корабля в пикселях 
 
-var canvas = document.getElementById("gameCanvas"); //ссылка на элемент по его ID
-var context = canvas.getContext("2d"); //контекст рисования на холсте
+var canvas = document.getElementById("gameCanvas"); // ссылка на элемент по его ID
+var context = canvas.getContext("2d"); // контекст рисования на холсте
 
-//установка игрового цикла
+var ship = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    r: SHIP_SIZE / 2,
+    a: 90 / 180 * Math.PI // конвертируем в радианы
+}
+
+// установка игрового цикла
 setInterval(update, 1000 / FPS);
 
 function update() {
-    //рисуем космос
+    // рисуем космос
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    //рисуем корабль
 
-    //поворот корабля
+    // рисуем треугольный корабль
+    context.strokeStyle = "white";
+    context.lineWidth = SHIP_SIZE / 20;
+    context.beginPath();
+    context.moveTo( // кончик корабля
+        ship.x + ship.r * Math.cos(ship.a),
+        ship.y - ship.r * Math.sin(ship.a) - 5
+    );
+    context.lineTo( // корма слева
+        ship.x - ship.r * (Math.cos(ship.a) + Math.sin(ship.a)),
+        ship.y + ship.r * (Math.sin(ship.a) - Math.cos(ship.a))
+    );
+    context.lineTo( // корма
+        ship.x - ship.r * (Math.cos(ship.a) - Math.sin(ship.a)),
+        ship.y + ship.r * (Math.sin(ship.a) + Math.cos(ship.a))
+    );
+    context.closePath();
+    context.stroke();
 
-    //движение корабля
+    // поворот корабля
+
+    // движение корабля
 
 }
