717c79e2def72a39ad2727f56936c66772c711a7
Бездетный Николай <Nekooolay@mail.ru>
2019-03-23 20:37:16 +0300

    Поворот космического корабля

--- logics.js
+++ logics.js
@@ -1,5 +1,6 @@
 const FPS = 30; // кадров в секунду
 const SHIP_SIZE = 30; // высота корабля в пикселях 
+const TURN_SPEED = 360; // скорость поворота градусов в секунду
 
 var canvas = document.getElementById("gameCanvas"); // ссылка на элемент по его ID
 var context = canvas.getContext("2d"); // контекст рисования на холсте
@@ -8,11 +9,44 @@ var ship = {
     x: canvas.width / 2,
     y: canvas.height / 2,
     r: SHIP_SIZE / 2,
-    a: 90 / 180 * Math.PI // конвертируем в радианы
+    a: 90 / 180 * Math.PI, // конвертируем в радианы
+    rot: 0
 }
+
+// установка событий
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
 // установка игрового цикла
 setInterval(update, 1000 / FPS);
+
+function keyDown(/** @type {KeyboardEvent} */ ev) {
+    switch(ev.keyCode) {
+        case 37: // левая стрелка (поворот корабля влево)
+            ship.rot = TURN_SPEED / 180 * Math.PI / FPS;
+            break;
+        case 38: // стрелка вверх (движение вперед)
+
+            break;
+        case 39: // правая стрелка (поворот корабля вправо)
+            ship.rot = -TURN_SPEED / 180 * Math.PI / FPS;
+            break;
+    }
+}
+
+function keyUp(/** @type {KeyboardEvent} */ ev) {
+    switch(ev.keyCode) {
+        case 37: // левая стрелка (остановка поворота корабля влево)
+            ship.rot = 0;
+            break;
+        case 38: // стрелка вверх (остановка движения вперед)
+
+            break;
+        case 39: // правая стрелка (остановка поворота корабля вправо)
+            ship.rot = 0;
+            break;
+    }
+}
 
 function update() {
     // рисуем космос
@@ -24,22 +58,27 @@ function update() {
     context.lineWidth = SHIP_SIZE / 20;
     context.beginPath();
     context.moveTo( // кончик корабля
-        ship.x + ship.r * Math.cos(ship.a),
-        ship.y - ship.r * Math.sin(ship.a) - 5
+        ship.x + 4 / 3 * ship.r * Math.cos(ship.a),
+        ship.y - 4 / 3 * ship.r * Math.sin(ship.a)
     );
     context.lineTo( // корма слева
-        ship.x - ship.r * (Math.cos(ship.a) + Math.sin(ship.a)),
-        ship.y + ship.r * (Math.sin(ship.a) - Math.cos(ship.a))
+        ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + Math.sin(ship.a)),
+        ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - Math.cos(ship.a))
     );
     context.lineTo( // корма
-        ship.x - ship.r * (Math.cos(ship.a) - Math.sin(ship.a)),
-        ship.y + ship.r * (Math.sin(ship.a) + Math.cos(ship.a))
+        ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - Math.sin(ship.a)),
+        ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + Math.cos(ship.a))
     );
     context.closePath();
     context.stroke();
 
     // поворот корабля
+    ship.a += ship.rot
 
     // движение корабля
+
+    //центральная точка
+    context.fillStyle = "red";
+    context.fillRect(ship.x - 1, ship.y - 1, 2, 2);
 
 }
